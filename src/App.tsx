07eded7955f46e60b981b1c63c6ec9f6c46c3f01
Youness-Ibrahim-Youness-55499 <32335145+Youness-Ibrahim-Youness-55499@users.noreply.github.com>
2026-07-29 23:15:32 +0200

    Protect authenticated workspace route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { PublicOnly, RequireAuth } from "./auth/RouteGuards";
 import { AuthPage } from "./pages/AuthPage";
 import { LandingPage } from "./pages/LandingPage";
 import { LegalPage } from "./pages/LegalPage";
+import { ProductHomePage } from "./pages/ProductHomePage";
 
 export function App() {
   return (
     <Routes>
       <Route element={<LandingPage />} path="/" />
-      <Route element={<AuthPage mode="signup" />} path="/signup" />
-      <Route element={<AuthPage mode="login" />} path="/login" />
+      <Route
+        element={
+          <PublicOnly>
+            <AuthPage mode="signup" />
+          </PublicOnly>
+        }
+        path="/signup"
+      />
+      <Route
+        element={
+          <PublicOnly>
+            <AuthPage mode="login" />
+          </PublicOnly>
+        }
+        path="/login"
+      />
+      <Route
+        element={
+          <RequireAuth>
+            <ProductHomePage />
+          </RequireAuth>
+        }
+        path="/app"
+      />
       <Route element={<LegalPage kind="privacy" />} path="/privacy" />
       <Route element={<LegalPage kind="terms" />} path="/terms" />
       <Route element={<LegalPage kind="legal-notice" />} path="/legal-notice" />
@@ -16,5 +40,3 @@ export function App() {
     </Routes>
   );
 }
-
-
