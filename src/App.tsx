1293b955f13597c3b0a6471fa7a2d288010afd42
Youness-Ibrahim-Youness-55499 <32335145+Youness-Ibrahim-Youness-55499@users.noreply.github.com>
2026-07-29 19:53:11 +0200

    Update src/App.tsx for legal pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AuthPage } from "./pages/AuthPage";
 import { LandingPage } from "./pages/LandingPage";
+import { LegalPage } from "./pages/LegalPage";
 
 export function App() {
   return (
@@ -8,6 +9,9 @@ export function App() {
       <Route element={<LandingPage />} path="/" />
       <Route element={<AuthPage mode="signup" />} path="/signup" />
       <Route element={<AuthPage mode="login" />} path="/login" />
+      <Route element={<LegalPage kind="privacy" />} path="/privacy" />
+      <Route element={<LegalPage kind="terms" />} path="/terms" />
+      <Route element={<LegalPage kind="legal-notice" />} path="/legal-notice" />
       <Route element={<Navigate replace to="/" />} path="*" />
     </Routes>
   );
